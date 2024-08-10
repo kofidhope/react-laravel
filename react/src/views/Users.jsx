c2f6c2dfbb17ebd37be2c7282169bdf4de69a80b
Kofi dhope <forsonbentum55@gmail.com>
2024-08-10 08:26:30 -0700

    new commit

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosClient from '../axios-client';
 
 export default function Users() {
+
+    const [users, setUsers]= useState([]);
+    const [loading, setLoading]=useState(false);
+
+    useEffect(()=>{
+        getUsers()
+    },[])
+
+    const getUser = ()=>{
+        setLoading(true)
+        axiosClient.get('/users')
+        .then(({data})=>{
+            setLoading(false)
+            console.log(data)
+        })
+        .catch(()=>{
+            setLoading(false);
+        })
+    }
+
+
   return (
     <div>
-      users
+
     </div>
   )
 }
